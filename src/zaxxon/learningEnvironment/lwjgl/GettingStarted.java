80268824bf9733f34f9a2fb817c6cc7bbbbf56c3
Philip Eagles <pxe778@cs.bham.ac.uk>
2019-01-21 13:15:40 +0000

    added square and line

--- src/zaxxon/learningEnvironment/lwjgl/GettingStarted.java
+++ src/zaxxon/learningEnvironment/lwjgl/GettingStarted.java
@@ -2,6 +2,7 @@ package zaxxon.learningEnvironment.lwjgl;
 
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
+import org.lwjgl.openvr.Texture;
 import org.lwjgl.system.*;
 
 import static org.lwjgl.glfw.GLFW.*;
@@ -13,12 +14,23 @@ import java.nio.*;
 public class GettingStarted {
 
 	private static GLFWErrorCallback errorCallback = GLFWErrorCallback.createPrint(System.err);
+	
+	private static int xOffset = 0;
+	private static int yOffset = 0;
 
 	private static GLFWKeyCallback keyCallback = new GLFWKeyCallback() {
 		@Override
 		public void invoke(long window, int key, int scancode, int action, int mods) {
 			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
 				glfwSetWindowShouldClose(window, true);
+			} else if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
+				yOffset -= 100;
+			} else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) {
+				yOffset += 100;
+			} else if (key == GLFW_KEY_LEFT && action == GLFW_PRESS) {
+				xOffset += 100;
+			} else if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) {
+				xOffset -= 100;
 			}
 		}
 	};
@@ -47,6 +59,8 @@ public class GettingStarted {
 
 		IntBuffer width = MemoryUtil.memAllocInt(1);
 		IntBuffer height = MemoryUtil.memAllocInt(1);
+		
+//		STBImage
 
 		while (!glfwWindowShouldClose(window)) {
 			glfwGetFramebufferSize(window, width, height);
@@ -55,7 +69,7 @@ public class GettingStarted {
 			width.rewind();
 			height.rewind();
 
-			glViewport(0, 0, width.get(), height.get());
+			glViewport(xOffset, yOffset, width.get(), height.get());
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			glMatrixMode(GL_PROJECTION);
@@ -65,6 +79,7 @@ public class GettingStarted {
 
 			glLoadIdentity();
 			glRotatef((float) glfwGetTime() * 50f, 0f, 0f, 1f);
+//			glRotatef((float) glfwGetTime() * 50f, (float) glfwGetTime() * 60f, (float) glfwGetTime() * 40f, (float) glfwGetTime() * 70f);
 
 			glBegin(GL_TRIANGLES);
 			glColor3f(1f, 0f, 0f);
@@ -74,6 +89,35 @@ public class GettingStarted {
 			glColor3f(0f, 0f, 1f);
 			glVertex3f(0f, 0.6f, 0f);
 			glEnd();
+			
+			glBegin(GL_LINE_STRIP);
+			glColor3f(1f,1f,0f);
+			glVertex3f(-0.8f, 0.8f, 0f);
+			glColor3f(0f,1f,1f);
+			glVertex3f(0.8f, 0.8f, 0f);
+			glEnd();
+			
+//			glBegin(GL_QUAD_STRIP);
+//			glColor3f(1f,0f,0f);
+//			glVertex2f(0.2f, 0.2f);
+//			glColor3f(0f,1f,0f);
+//			glVertex2f(-0.2f, 0.2f);
+//			glColor3f(0f,0f,1f);
+//			glVertex2f(-0.2f, -0.2f);
+//			glColor3f(1f,1f,1f);
+//			glVertex2f(0.2f, -0.2f);
+//			glEnd();
+			
+			glBegin(GL_QUADS);
+			glColor4f(1f,0f,0f,0.5f);
+			glVertex2f(0.3f, 0.3f);
+			glColor4f(0f,1f,0f,0.5f);
+			glVertex2f(-0.3f, 0.3f);
+			glColor4f(0f,0f,1f,0.5f);
+			glVertex2f(-0.3f, -0.3f);
+			glColor4f(1f,1f,1f,0.5f);
+			glVertex2f(0.3f, -0.3f);
+			glEnd();
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
@@ -81,7 +125,7 @@ public class GettingStarted {
 			width.flip();
 			height.flip();
 		}
-		
+
 		MemoryUtil.memFree(width);
 		MemoryUtil.memFree(height);
 
