ecbd5e2407ef726552ef70e8c962ef943a109274
Philip Eagles <pxe778@cs.bham.ac.uk>
2019-01-31 12:06:43 +0000

    initial graphics demo

---
+++ CubeFace/src/com/zaxxon/client/ClientDemo.java
@@ -0,0 +1,82 @@
+package com.zaxxon.client;
+
+
+import java.util.*;
+
+import javafx.animation.*;
+import javafx.application.*;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.input.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.stage.*;
+
+
+public class ClientDemo extends Application {
+
+	private int xPos = 0;
+
+	private Set<KeyCode> keysPressed = new HashSet<KeyCode>();
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Demo");
+
+		Group root = new Group();
+		Group background = new Group();
+		Group foreground = new Group();
+		root.getChildren().add(background);
+		root.getChildren().add(foreground);
+
+		Scene renderedScene = new Scene(root, 800, 600);
+
+		Rectangle puppyRect = new Rectangle(128*2, 128);
+		Image puppyImg = new Image("/com/zaxxon/gameart/brute-zombie.png");
+		ImagePattern imgPat = new ImagePattern(puppyImg);
+		puppyRect.setFill(imgPat);
+
+		foreground.getChildren().add(puppyRect);
+
+		root.setFocusTraversable(true);
+		root.requestFocus();
+
+		primaryStage.setScene(renderedScene);
+		primaryStage.show();
+
+		primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent key) {
+				keysPressed.add(key.getCode());
+			}
+		});
+		
+		primaryStage.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent key) {
+				keysPressed.remove(key.getCode());
+			}
+		});
+
+		AnimationTimer animator = new AnimationTimer() {
+			public void handle(long currentNanoTime) {
+				dealWithKeys();
+				puppyRect.setX(xPos);
+			}
+		};
+		animator.start();
+	}
+
+	private void dealWithKeys() {
+		if (keysPressed.contains(KeyCode.LEFT)) {
+			xPos -= 1;
+		}
+		if (keysPressed.contains(KeyCode.RIGHT)) {
+			xPos += 1;
+		}
+	}
+
+}
