0b4cdf5ebc91d31b04852ccc65c558a940a16394
Philip Eagles <pxe778@cs.bham.ac.uk>
2019-01-31 16:29:38 +0000

    added Sprite spritesheet support

--- CubeFace/src/com/zaxxon/world/mobile/MovableSprite.java
+++ CubeFace/src/com/zaxxon/world/mobile/MovableSprite.java
@@ -3,71 +3,68 @@ package com.zaxxon.world.mobile;
 import com.zaxxon.world.Sprite;
 
 public class MovableSprite extends Sprite {
-    private double velocityX;
-    private double velocityY;
-    private double movementSpeed;
-    private double health;
-    private double damage;
-    public boolean controllable;
+	private double velocityX;
+	private double velocityY;
+	private double movementSpeed;
+	private double health;
+	private double damage;
+	public boolean controllable;
 
-    public MovableSprite(){
-        velocityX = 0.0;
-        velocityY = 0.0;
-        movementSpeed = 0.0;
-        health = 0.0;
-        damage = 0.0;
-        positionX = 0;
-        positionY = 0;
-    }
+	public MovableSprite() {
+		velocityX = 0.0;
+		velocityY = 0.0;
+		movementSpeed = 0.0;
+		health = 0.0;
+		damage = 0.0;
+		positionX = 0;
+		positionY = 0;
+	}
 
-    public void setVelocity(double x, double y)
-    {
-        velocityX = x;
-        velocityY = y;
-    }
+	public void setVelocity(double x, double y) {
+		velocityX = x;
+		velocityY = y;
+	}
 
-    public void addVelocity(double x, double y)
-    {
-        velocityX += x;
-        velocityY += y;
-    }
+	public void addVelocity(double x, double y) {
+		velocityX += x;
+		velocityY += y;
+	}
 
-    public void update(double time)
-    {
-        this.positionX += velocityX * time;
-        this.positionY += velocityY * time;
-    }
+	public void update(double time) {
+		this.positionX += velocityX * time;
+		this.positionY += velocityY * time;
+	}
 
-    public void takeDamage(int damage){
-        health -= damage;
-    }
+	public void takeDamage(int damage) {
+		health -= damage;
+	}
 
-    public void heal(int healing){
-        health += healing;
-    }
+	public void heal(int healing) {
+		health += healing;
+	}
 
-    private double getHealth(){
-        return health;
-    }
+	private double getHealth() {
+		return health;
+	}
 
-    public void moveLeft(){
-        addVelocity(-50,0);
-    }
+	public void moveLeft() {
+		addVelocity(-50, 0);
+	}
 
-    public void moveUp(){
-        addVelocity(0,50);
-    }
+	public void moveUp() {
+		addVelocity(0, 50);
+	}
 
-    public void moveRight(){
-        addVelocity(50,0);
-    }
+	public void moveRight() {
+		addVelocity(50, 0);
+	}
 
-    public void moveDown(){
-        addVelocity(0,-50);
-    }
+	public void moveDown() {
+		addVelocity(0, -50);
+	}
 
-    public String toString(){
-        return super.toString() + " Velocity: [" + velocityX + "," + velocityY + "]";
-    }
+	public String toString() {
+		return super.toString() + ", Velocity: [" + velocityX + "," + velocityY + "]";
+	}
 
 }
