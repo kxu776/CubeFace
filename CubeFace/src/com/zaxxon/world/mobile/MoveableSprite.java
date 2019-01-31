8173cd7c34a683e327b616f682262905d8b66f83
kUjejski <kxu776@student.bham.ac.uk>
2019-01-31 14:37:05 +0000

    Added health system method to enemy classes

--- CubeFace/src/com/zaxxon/world/mobile/MoveableSprite.java
+++ CubeFace/src/com/zaxxon/world/mobile/MoveableSprite.java
@@ -3,8 +3,55 @@ package com.zaxxon.world.mobile;
 import com.zaxxon.world.Sprite;
 
 public class MoveableSprite extends Sprite {
-    protected double movementSpeed;
-    protected double health;
-    protected double damage;
+    private double velocityX;
+    private double velocityY;
+    private double movementSpeed;
+    private double health;
+    private double damage;
     protected boolean controllable;
+
+    public MoveableSprite(){
+        velocityX = 0.0;
+        velocityY = 0.0;
+        movementSpeed = 0.0;
+        health = 0.0;
+        damage = 0.0;
+        positionX = 0;
+        positionY = 0;
+    }
+
+    public void setVelocity(double x, double y)
+    {
+        velocityX = x;
+        velocityY = y;
+    }
+
+    public void addVelocity(double x, double y)
+    {
+        velocityX += x;
+        velocityY += y;
+    }
+
+    public void update(double time)
+    {
+        this.positionX += velocityX * time;
+        this.positionY += velocityY * time;
+    }
+
+    public void takeDamage(int damage){
+        health -= damage;
+    }
+
+    public void heal(int healing){
+        health += healing;
+    }
+
+    private double getHealth(){
+        return health;
+    }
+
+    public String toString(){
+        return super.toString() + " Velocity: [" + velocityX + "," + velocityY + "]";
+    }
+
 }
