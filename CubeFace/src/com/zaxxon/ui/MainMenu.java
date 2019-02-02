ce6a432c36959d1cddf51597677fd193404bb02b
mmp725 <mmp725@student.bham.ac.uk>
2019-02-02 16:13:02 +0000

    added updated ui files

--- CubeFace/src/com/zaxxon/ui/MainMenu.java
+++ CubeFace/src/com/zaxxon/ui/MainMenu.java
@@ -6,18 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainMenu extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("cubeface");
+        primaryStage.setTitle("CubeFace");
         primaryStage.setScene(new Scene(root, 600, 575));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
