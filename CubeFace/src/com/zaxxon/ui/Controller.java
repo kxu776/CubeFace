065f0e8ed166506df4546a86c4f556732e3b1b34
mmp725 <mmp725@student.bham.ac.uk>
2019-02-10 18:25:10 +0000

    redone ui not using fxml file

--- CubeFace/src/com/zaxxon/ui/Controller.java
+++ CubeFace/src/com/zaxxon/ui/Controller.java
@@ -1,18 +1,34 @@
 package com.zaxxon.ui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 public class Controller {
     public Label startGame;
     public Label openAudioMenu;
     public Label openHelpScreen;
-    public Image logo;
+
+    //load the cubefacelogo
+    Image image = new Image(new FileInputStream("C:\\Users\\Megan\\git\\zaxxon\\CubeFace\\src\\com\\zaxxon\\ui\\cubefacelogo.png"));
+
+    public Controller() throws FileNotFoundException {
+    }
+
+    //set the imageview
+    ImageView imageView = new ImageView(image);
+
+
+
 
     public void startGame(ActionEvent actionEvent) {
         startGame.setText("Starting game...");
@@ -26,18 +42,9 @@ public class Controller {
         openHelpScreen.setText("Opening Help Screen...");
     }
 
-    public void initialiseLogo() {
-        File file = new File("src/sample/cubefacelogo.png");
-        Image logo = new Image(file.toURI().toString());
 
-    }
 }
 
 
-   /* @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        File file = new File("src/Box13.jpg");
-        Image image = new Image(file.toURI().toString());
-        imageView.setImage(image);*/
 
 
--- CubeFace/src/com/zaxxon/ui/MainMenu.java
+++ CubeFace/src/com/zaxxon/ui/MainMenu.java
@@ -2,23 +2,63 @@ package com.zaxxon.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 public class MainMenu extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage stage) throws Exception{
+
+        //load cubeface logo
+
+        //make a group
+
+
+
+
+
+
 
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         root.getStylesheets().add(getClass().getResource("ms.css").toString());
 
-        primaryStage.setTitle("CubeFace");
-        primaryStage.setScene(new Scene(root, 600, 575));
-        primaryStage.show();
+        //create a window for the ui (a stage)
+        stage.setTitle("CubeFace");
+        stage.setScene(new Scene(root, 600, 575));
+
+
+        //Load the logo image
+        //Create the image
+        Image logo = new Image(new FileInputStream("C:\\Users\\Megan\\git\\zaxxon\\CubeFace\\src\\com\\zaxxon\\ui\\cubefacelogo.png"));
+
+        //set the image view
+        ImageView imageView = new ImageView(logo);
+
+        //set the position of the image
+        imageView.setX(50);
+        imageView.setY(25);
+
+        //setting the fit height and width of the image view
+        imageView.setFitHeight(455);
+        imageView.setFitWidth(500);
+
+        //Setting the preserve ratio of the image view
+        imageView.setPreserveRatio(true);
+
+        //create a group for the image
+        Group logoGroup = new Group(imageView);
+
+
+        stage.show();
     }
 
     public static void main(String[] args) {
