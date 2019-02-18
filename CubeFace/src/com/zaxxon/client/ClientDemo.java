c29c198a13d58219b0f7e106a94ead98c7dbcc7e
oxf612 <oxf612@student.bham.ac.uk>
2019-02-18 13:34:34 +0000

    Should work now

---
+++ CubeFace/src/com/zaxxon/client/ClientDemo.java
@@ -0,0 +1,72 @@
+package com.zaxxon.client;
+
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+
+import javax.imageio.*;
+
+import com.zaxxon.input.Input;
+import com.zaxxon.world.Sprite;
+import com.zaxxon.world.mobile.Player;
+
+import javafx.animation.*;
+import javafx.application.*;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.input.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.stage.*;
+
+public class ClientDemo extends Application {
+
+	private int xPos = 0;
+	private Player player;
+	private static Input input;
+	private int zombieImage = 0;
+
+	public Set<KeyCode> keysPressed = new HashSet<KeyCode>();
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Demo");
+
+		Group root = new Group();
+		Group background = new Group();
+		Group foreground = new Group();
+		root.getChildren().add(background);
+		root.getChildren().add(foreground);
+
+		Scene renderedScene = new Scene(root, 800, 600);
+		
+		
+		player = new Player();
+		foreground.getChildren().add(player);
+		
+
+		root.setFocusTraversable(true);
+		root.requestFocus();
+
+		primaryStage.setScene(renderedScene);
+		primaryStage.show();
+		
+		Input.addHandlers(primaryStage);
+
+		AnimationTimer animator = new AnimationTimer() {
+			
+			public void handle(long currentNanoTime) {
+				
+				player.update(1);
+			}
+		};
+		animator.start();
+	}
+
+}
